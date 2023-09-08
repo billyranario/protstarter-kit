22cf5fc9c2d853ba9be3f66736eb0e107fce4e9e
billy joel ranario <billy.ranario@ustp.edu.ph>
2023-09-08 23:41:07 +0800

    updated config passport client name

--- src/Config/prostarter-kit.php
+++ src/Config/prostarter-kit.php
@@ -9,13 +9,13 @@ return [
     | This config requires laravel passport to be installed.
     |
     | to generate `sail artisan passport:client --password`
-    | set password grant client name to: pfs-password-grant
+    | set password grant client name to: psk-password-grant
     |
     | to generate `sail artisan passport:client --personal`
-    | set password grant client name to: pfs-personal-access
+    | set password grant client name to: psk-personal-access
 
     |
     */
-    'password_grant' => 'pfs-password-grant',
-    'personal_access' => 'pfs-personal-access',
+    'password_grant' => 'psk-password-grant',
+    'personal_access' => 'psk-personal-access',
 ];
