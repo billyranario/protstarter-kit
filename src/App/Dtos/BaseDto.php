606bf2c90eabd1a1eb9c762859e3cbff51cb1cab
billy joel ranario <billy.ranario@ustp.edu.ph>
2025-04-13 10:53:12 +0800

    added new properties on baseDto

--- src/App/Dtos/BaseDto.php
+++ src/App/Dtos/BaseDto.php
@@ -6,19 +6,25 @@ class BaseDto
 {
     /**
      * @var int|null $id
+     * @var array $ids
      * @var int $perPage
      * @var int $page
      * @var string $orderBy
      * @var string $orderDirection
      * @var string $searchKeyword
      * @var array $relations
+     * @var bool $forceDelete
+     * @var bool $isArchive
      */
     private ?int $id = null;
+    private array $ids = [];
     private int $perPage = 10;
     private string $orderBy = 'created_at';
     private string $orderDirection = 'desc';
     private ?string $searchKeyword = null;
     private array $relations = [];
+    private bool $forceDelete = false;
+    private bool $isArchive = false;
 
     /**
      * @param int|null $id
@@ -26,6 +32,14 @@ class BaseDto
     public function setId(?int $id): void
     {
         $this->id = $id;
+    }
+
+    /**
+     * @param array $ids
+     */
+    public function setIds(array $ids): void
+    {
+        $this->ids = $ids;
     }
 
     /**
@@ -69,11 +83,35 @@ class BaseDto
     }
 
     /**
+     * @param bool $forceDelete
+     */
+    public function setForceDelete(bool $forceDelete): void
+    {
+        $this->forceDelete = $forceDelete;
+    }
+
+    /**
+     * @param bool $isArchive
+     */
+    public function setIsArchive(bool $isArchive): void
+    {
+        $this->isArchive = $isArchive;
+    }
+
+    /**
      * @return int|null
      */
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return array
+     */
+    public function getIds(): array
+    {
+        return $this->ids;
     }
 
     /**
@@ -114,5 +152,21 @@ class BaseDto
     public function getRelations(): array
     {
         return $this->relations;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isForceDelete(): bool
+    {
+        return $this->forceDelete;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isArchive(): bool
+    {
+        return $this->isArchive;
     }
 }
